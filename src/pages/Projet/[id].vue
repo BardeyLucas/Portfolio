fd4b04186c987380636d8e35652a5371591a1366
Barder_Lucas <bardey.estelle1@gmail.com>
2024-10-31 21:27:25 +0100

    Mise à jour des dépendances et ajout de la bibliothèque sanitize-html

--- src/pages/Projet/[id].vue
+++ src/pages/Projet/[id].vue
@@ -1,4 +1,22 @@
 <!-- eslint-disable vue/multi-word-component-names -->
+<script setup lang="ts">
+import { pb } from '@/backend'
+import { ref } from 'vue';
+import { useHead } from '@unhead/vue'
+import { useRoute, useRouter } from 'vue-router/auto';
+import sanitizeHtml from 'sanitize-html'
+import type { ProjetsResponse } from '@/pocketbase-types';
+const route = useRoute('/Projet/[id]')
+const router = useRouter()
+const Projet = ref(
+    await pb
+        .collection<ProjetsResponse>('Projet')
+        .getOne(route.params.id)
+)
+useHead({
+    title: () => `Projet : ${Projet.value.title}`
+})
+</script>
 <template>
     <p>projet</p>
 </template>

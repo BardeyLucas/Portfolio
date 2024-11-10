b999733228667480ac725e1bdfae1bbfda2afb42
Barder_Lucas <bardey.estelle1@gmail.com>
2024-11-10 16:43:28 +0100

    Graphisme footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,10 +1,10 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <footer>
-        <div class="grille">
+    <footer class="bg-Background border-t-Rose border-t-2 text-Blanc">
+        <div class="grille pb-5">
             <div class="col-span-12 lg:col-start-2 lg:col-span-2 text-center lg:text-left">
-                <h3 class="text-center">Contacts</h3>
-                <nav>
+                <h3 class="flex items-center justify-center h-24 font-michroma text-2xl">Contacts</h3>
+                <nav class="gap-y-3 flex flex-col text-lg">
                     <RouterLink to="/formulaire">Formulaire</RouterLink>
                     <p>+33 6 32 34 98 88</p>
                     <p>Bardey.lucas1@gmail.com</p>
@@ -12,15 +12,16 @@
                 </nav>
             </div>
             <div class="col-span-12 lg:col-start-5 lg:col-span-2 text-center lg:text-left">
-                <h3 class="text-center">Réseaux</h3>
-                <nav>
+                <h3 class="flex items-center justify-center h-24 font-michroma text-2xl">Réseaux</h3>
+                <nav class="gap-y-3 flex flex-col text-lg">
                     <a href="#"><p>- Linkedin</p></a>
+                    <a href="#"><p>- GitHub</p></a>
                     <a href="#"><p>- Instagram</p></a>
                 </nav>
             </div>
             <div class="col-span-12 lg:col-start-8 lg:col-span-2 text-center lg:text-left">
-                <h3 class="text-center">Projets</h3>
-                <nav>
+                <h3 class="flex items-center justify-center h-24 font-michroma text-2xl">Projets</h3>
+                <nav class="gap-y-3 flex flex-col text-lg">
                     <RouterLink to="#"><p>- Cardeleon</p></RouterLink>
                     <RouterLink to="#"><p>- Architecture</p></RouterLink>
                     <RouterLink to="#"><p>- Logo</p></RouterLink>
@@ -28,7 +29,7 @@
                 </nav>
             </div>
         </div>
-        <div class="text-center"><RouterLink to="/Mentions_Legales"><p>Mentions légales</p></RouterLink></div>
-        <div class="text-center"><p>@ 2024 Lucas Bardey all rights reserved</p></div>
+        <div class="flex items-center justify-center h-16 border-t-Rose border-t-2 text-lg"><RouterLink to="/Mentions_Legales"><p>Mentions légales</p></RouterLink></div>
+        <div class="flex items-center justify-center h-16 border-t-Rose border-t-2 text-lg"><p>@ 2024 Lucas Bardey all rights reserved</p></div>
     </footer>
     </template>

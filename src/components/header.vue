2ee53a5af9e66704da0dbd7c455d5a0ade2eb6f5
Barder_Lucas <bardey.estelle1@gmail.com>
2024-11-18 11:40:23 +0100

    Ajout d'une navigation stylisé

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,6 +3,7 @@
 <script setup lang="ts">
 import { ref, reactive, computed } from 'vue';
 import { RouterLink } from 'vue-router';
+import Croix from './icons/Croix.vue';
 // const language = reactive({
 //   lang: false, // false pour FR, true pour EN
 // });
@@ -11,7 +12,7 @@ const lang = ref(false)
 import { useRoute } from 'vue-router';
 
 const route = useRoute();
-
+const activeMenu = ref(false)
 const isNotHomePage = computed(() => route.path != '/');
 
 </script>
@@ -24,35 +25,38 @@ const isNotHomePage = computed(() => route.path != '/');
             </RouterLink>
             <nav class="flex gap-5">
                 <button><span @click="lang = true" :class="{'!font-semibold':lang}">EN</span>/<span class="font-semibold" @click="lang = false" :class="{'!font-normal':lang}">FR</span></button>
-                <button>Menu</button>
+                <button @click="activeMenu =! activeMenu">Menu</button>
             </nav>
         </nav>
-        <nav class="fixed bg-Rose right-0 hidden" style="margin-right: calc((100vw - 1420px)/2);">
-            <nav class="flex gap-5">
+        <nav class="absolute h-screen -right-12 pl-12 pr-24 text-right hidden" :class="{'!block':activeMenu}" style="margin-right: calc((100vw - 100%)/2); background: linear-gradient(to bottom, #00092a 0%, rgba(0,9,42,0.9) 30%, rgba(0,9,42,0) 100%);">
+            <nav class="flex justify-between gap-5 w-auto h-40">
                 <button><span @click="lang = true" :class="{'!font-semibold':lang}">EN</span>/<span class="font-semibold" @click="lang = false" :class="{'!font-normal':lang}">FR</span></button>
-                <button>Menu</button>
+                <button class="block lg:hidden" @click="activeMenu =! activeMenu">Menu</button>
+                <button class="hidden lg:block" @click="activeMenu =! activeMenu"><Croix/></button>
             </nav>
-            <RouterLink to="/Projet">
-                <p>Work</p>
-            </RouterLink>
-            <RouterLink to="/Projet/hzyx8uz709k7b6w">
-                <p>Cardeleon</p>
-            </RouterLink>
-            <RouterLink to="/Projet/3any1equfk6yz38">
-                <p>Redesign architectural</p>
-            </RouterLink>
-            <RouterLink to="/Projet">
-                <p>Site pour un tournois Esport</p>
-            </RouterLink>
-            <RouterLink to="/Projet/6ym7rewppwl81f4">
-                <p>Randoala</p>
-            </RouterLink>
-            <RouterLink to="/Mentions_Legales">
-                <p>A propos</p>
-            </RouterLink>
-            <RouterLink to="/formulaire">
-                <p>Contact</p>
-            </RouterLink>
+            <nav class="flex flex-col gap-7">
+                <RouterLink to="/Projet">
+                    <p>Work</p>
+                </RouterLink>
+                <RouterLink to="/Projet/hzyx8uz709k7b6w">
+                    <p>Cardeleon</p>
+                </RouterLink>
+                <RouterLink to="/Projet/3any1equfk6yz38">
+                    <p>Redesign architectural</p>
+                </RouterLink>
+                <RouterLink to="/Projet">
+                    <p>Site pour un tournois Esport</p>
+                </RouterLink>
+                <RouterLink to="/Projet/6ym7rewppwl81f4">
+                    <p>Randoala</p>
+                </RouterLink>
+                <RouterLink to="/Mentions_Legales">
+                    <p>A propos</p>
+                </RouterLink>
+                <RouterLink to="/formulaire">
+                    <p>Contact</p>
+                </RouterLink>
+            </nav>
         </nav>
     </header>
 </template>
---
+++ src/components/icons/Croix.vue
@@ -0,0 +1,7 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+<svg width="27" height="28" viewBox="0 0 27 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect y="2.68629" width="3" height="35" transform="rotate(-45 0 2.68629)" fill="#F4F7FF"/>
+<rect x="2.12134" y="27.435" width="3" height="35" transform="rotate(-135 2.12134 27.435)" fill="#F4F7FF"/>
+</svg>
+</template>

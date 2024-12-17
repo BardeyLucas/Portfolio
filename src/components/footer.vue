9f4933e29e530f208deccf2f2871ce15092d94cd
Barder_Lucas <bardey.estelle1@gmail.com>
2024-12-17 20:19:50 +0100

    Button reponsive

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -5,15 +5,16 @@ import BackArrow from './icons/Retour-en-arriere.vue';
 <template>
     <footer class="bg-Background border-t-Rose border-t text-Blanc">
         <div class="grille pb-5">
-            <div class="col-span-12 lg:col-start-2 lg:col-span-2 text-center lg:text-left">
+            <div class="col-start-3 col-span-8 lg:col-start-2 lg:col-span-2 text-center lg:text-left">
                 <h3 class="flex items-center justify-center mt-12 mb-6 lg:my-0 lg:h-24 font-michroma text-lg lg:text-2xl">Contacts</h3>
                 <nav class="gap-y-3 flex flex-col lg:text-lg">
                     <RouterLink to="/formulaire" onclick="window.scrollTo(0, 0);">Formulaire</RouterLink>
                     <a href="tel:+33632349884">+33 6 32 34 98 84</a>
                     <a href="mailto:bardey.lucas1@gmail.com">Bardey.lucas1@gmail.com</a>
-                    <a href="https://www.google.com/maps/place/55+Rue+Charles+de+Gaulle,+90500+Beaucourt/@47.4819805,6.9196241,15z" class="px-10 lg:px-0">55 rue Charles de Gaulle 90500 Beaucourt, France</a>
+                    <a href="https://www.google.com/maps/place/55+Rue+Charles+de+Gaulle,+90500+Beaucourt/@47.4819805,6.9196241,15z">55 rue Charles de Gaulle 90500 Beaucourt, France</a>
                 </nav>
             </div>
+            <BackArrow onclick="window.scrollTo({ top: 0, behavior: 'smooth' });" class="mt-10 w-9 h-9 col-start-11 col-span-1 lg:hidden"/>
             <div class="col-span-12 lg:col-start-5 lg:col-span-2 text-center lg:text-left">
                 <h3 class="flex items-center justify-center mt-12 mb-6 lg:my-0 lg:h-24 font-michroma text-lg lg:text-2xl">Réseaux</h3>
                 <nav class="gap-y-3 flex flex-col lg:text-lg">
@@ -31,7 +32,7 @@ import BackArrow from './icons/Retour-en-arriere.vue';
                     <RouterLink to="/Projet/6ym7rewppwl81f4" onclick="window.scrollTo(0, 0);"><p>Randoala</p></RouterLink>
                 </nav>
             </div>
-            <BackArrow onclick="window.scrollTo({ top: 0, behavior: 'smooth' });" class="mt-12 col-start-11 col-span-1"/>
+            <BackArrow onclick="window.scrollTo({ top: 0, behavior: 'smooth' });" class="mt-12 col-start-11 col-span-1 hidden lg:block"/>
         </div>
         <div class="flex items-center justify-center h-12 lg:h-16 border-t-Rose border-t text-xs lg:text-lg font-michroma"><RouterLink to="/Mentions_Legales" onclick="window.scrollTo(0, 0);"><p>Mentions légales</p></RouterLink></div>
         <div class="flex items-center justify-center h-12 lg:h-16 border-t-Rose border-t text-xs lg:text-lg font-michroma text-center"><p>@ 2024 Lucas Bardey all rights reserved</p></div>

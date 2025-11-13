49f92aa728865a6d01aef7b53f7a2b940e432f03
Lucas Bardey <bardey.estelle1@gmail.com>
2025-11-13 18:32:44 +0100

    Ajout des sections projets (prototypes)

--- src/components/ProjectPresentation.vue
+++ src/components/ProjectPresentation.vue
@@ -1,11 +1,48 @@
-<template class="col-span-12">
-    <article class="flex">
-        <section class="h-48 w-48 bg-Rose">
-
+<template>
+    <article :class="{'flex-row-reverse justify-between': Direction == 'reverse'}" class="col-span-12 flex  gap-x-10 bg-[#FFFFFF20] rounded-2xl px-10 py-6">
+        <section class="h-72"> 
+            <img v-if="Image == '1'" class="h-full w-full rounded-2xl" src="./ProjetImage/Image-Smash-Arena-Event.png" alt="Smash Arena Event" />  
+            <img v-if="Image == '2'" class="h-full w-full rounded-2xl" src="./ProjetImage/Image-Sainte-Suzanne.png" alt="Sainte Suzanne" />  
         </section>
-        <section>
-            <h2>PlaceHolder</h2>
-            <p>Le texte varira selon le projet</p>
+        <section class="gap-y-4 flex flex-col">
+            <h2 class="font-michroma text-lg lg:text-2xl">{{Title}}</h2>
+            <div class="flex gap-x-4 font-medium flex-wrap"> 
+                <p v-if="Param1" class="bg-Bleu px-3 py-1 rounded-3xl text-[#000]">{{Param1}}</p>
+                <p v-if="Param2" class="bg-Bleu px-3 py-1 rounded-3xl text-[#000]">{{Param2}}</p>
+                <p v-if="Param3" class="bg-Bleu px-3 py-1 rounded-3xl text-[#000]">{{Param3}}</p>
+                <p v-if="Param4" class="bg-Bleu px-3 py-1 rounded-3xl text-[#000]">{{Param4}}</p>
+                <p v-if="Param5" class="bg-Bleu px-3 py-1 rounded-3xl text-[#000]">{{Param5}}</p>
+            </div>
+            <p>{{Description}}</p>
+            <div class="flex flex-col gap-y-2 font-medium">
+                <p>Logiciels et outils majoritairement utilisés :</p>
+                <div class="flex gap-x-4 flex-wrap opacity-80"> 
+                    <p v-if="Outil1" class="bg-Bleu px-2 py-0.5 rounded-3xl text-[#000]">{{Outil1}}</p>
+                    <p v-if="Outil2" class="bg-Bleu px-2 py-0.5 rounded-3xl text-[#000]">{{Outil2}}</p>
+                    <p v-if="Outil3" class="bg-Bleu px-2 py-0.5 rounded-3xl text-[#000]">{{Outil3}}</p>
+                    <p v-if="Outil4" class="bg-Bleu px-2 py-0.5 rounded-3xl text-[#000]">{{Outil4}}</p>
+                    <p v-if="Outil5" class="bg-Bleu px-2 py-0.5 rounded-3xl text-[#000]">{{Outil5}}</p>
+                </div>
+            </div>
         </section>
     </article>
 </template>
+<script setup lang="ts">
+    import { defineProps } from 'vue'
+    const props = defineProps<{
+        Image: string
+        Title: string
+        Description: string
+        Direction?: string
+        Param1?: string
+        Param2?: string
+        Param3?: string
+        Param4?: string
+        Param5?: string
+        Outil1?: string
+        Outil2?: string
+        Outil3?: string
+        Outil4?: string
+        Outil5?: string
+    }>()
+</script>
--- src/pages/Projet/AutresProjets.vue
+++ src/pages/Projet/AutresProjets.vue
@@ -4,8 +4,11 @@ import ProjectPresentation from '@/components/ProjectPresentation.vue';
 </script>
 
 <template>
-    <div class="grille">
-        <h1 class="col-span-12 mt-8 pb-6 font-michroma text-xl lg:text-4xl">Les projets les plus récents</h1>
-    </div>
-    <ProjectPresentation />
+    <section class="grille">
+        <h1 class="col-span-12 mt-8 pb-8 font-michroma text-xl lg:text-4xl">Les projets les plus récents</h1>
+        <section class="col-span-12 flex flex-col gap-y-12 mb-20">
+            <ProjectPresentation Image="1" Title="SMASH ARENA EVENT 2éme édition | 2025" Description="Description du projet" Direction="" Param1="Tournoi Esport" Param2="Projet collectif" Param3="Organisation Evenementiel" Param4="Développement d'un site promotionnel" Param5="Communication" Outil1="Figma" Outil2="Vs code" Outil3="Suite Google" Outil4="Premiere Pro" Outil5="After Effects"/>
+            <ProjectPresentation Image="2" Title="Stage d'infographiste à la mairie de Sainte Suzanne" Description="Description du projet" Direction="reverse"/>
+        </section>
+    </section>
 </template>

a7e32fabbc44d451c83b07dd8e8bafc1d3c6e3dc
Lucas Bardey <bardey.estelle1@gmail.com>
2025-11-14 07:41:14 +0100

    Ajout fonctionnalité (suppression de banderoll à l"ouverture de la pge autres projets)

--- src/components/ButtonOtherProject.vue
+++ src/components/ButtonOtherProject.vue
@@ -1,8 +1,18 @@
 <script setup lang="ts">
-    import { RouterLink } from 'vue-router';
+    import { RouterLink, useRoute } from 'vue-router';
+    import { computed } from 'vue';
+    const route = useRoute();
+    const Page_actuelle = computed(() => {
+        switch (route.path) {
+            case '/Projet/AutresProjets':
+                return false;
+            default:
+                return true;
+        }
+    });
 </script>
 <template>
-    <div class="text-Blanc fixed z-50 bg-[#1c0834] border-2 border-Bleu w-screen bottom-0 lg:text-2xl px-5 py-4 opacity-90">
+    <div class="text-Blanc fixed z-50 bg-[#1c0834] border-2 border-Bleu w-screen bottom-0 lg:text-2xl px-8 py-6 opacity-90 font-semibold" :class="{'!hidden':!Page_actuelle}">
         <p>Les projets les plus récents n'ont pas encore de pages apparentières dans le portfolio et seront ajouter ultérieurement. Les projets et travaux récents sont donc répértoriés
             <RouterLink to="/Projet/AutresProjets" class="">
                 <button class="text-Bleu underline underline-offset-4">[ici]</button>
